afe07d6cbd7c38680e7eeabe92c3db48208e102c
jungwoo0716 <kimjungwoo0716@gmail.com>
2024-01-07 22:48:23 +0900

    gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ try {
 
 if (
     process.env.NODE_ENV === `production` &&
-    config.siteUrl === `http://localhost:8000` &&
+    config.siteUrl === `https://jungwoo0716.github.io/` &&
     !process.env.SITEURL
 ) {
     throw new Error(
