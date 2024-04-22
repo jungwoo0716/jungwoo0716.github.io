a30343bedf2c8da7cdceabed7d2be4f6ce73bb53
Kim JungWoo <kimjungwoo0716@gmail.com>
2024-04-22 05:32:00 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d79f5cd69b082b30091d.js"
+    "url": "webpack-runtime-a9d17e1c3748b1abac3e.js"
   },
   {
     "url": "framework-857450928436282c32b9.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ee2d918417b36375b08f3fefc615db10"
+    "revision": "8093b6849a08e779ce72b544aeb9a375"
   },
   {
     "url": "manifest.webmanifest",
